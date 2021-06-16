cd8b6c77ae9c02e5c90d449dd33215cc3c361c9f
alumno <alumno@192.168.56.1>
2021-06-16 17:18:17 +0200

    ejercicio5 del examen

--- src/fp/daw/exprog20210616/ejercicio5/Ejercicio5.java
+++ src/fp/daw/exprog20210616/ejercicio5/Ejercicio5.java
@@ -2,14 +2,38 @@ package fp.daw.exprog20210616.ejercicio5;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Ejercicio5 extends JFrame {
+public class Ejercicio5 extends JFrame implements ActionListener{
+	
+	private static JMenuItem opcionAbrir;
+	private static JFileChooser elegir;
+	private Queue<Integer> q = new LinkedList<Integer>();
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -25,21 +49,68 @@ public class Ejercicio5 extends JFrame {
 	}
 		
 	public Ejercicio5() {
+		super("Examen Final-Ejercicio5");
 		setTitle("Examen Final-Ejercicio5");
 		setVisible(true);
+		setBounds(100,100, 800, 500);
 		JPanel panel = (JPanel) getContentPane();
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		panel.setLayout(new BorderLayout());
-		//barra de menu
+		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+		
+		//MENU
 		JMenuBar barra = new JMenuBar();
 		JMenu menu = new JMenu("Archivo"); 
-		JMenuItem opcionAbrir = new JMenuItem("Abrir");
+		menu.setMnemonic(KeyEvent.VK_M);
+		opcionAbrir = new JMenuItem("Abrir");
+		opcionAbrir.setMnemonic(KeyEvent.VK_A);
 		menu.add(opcionAbrir);
 		barra.add(menu); 
 		setJMenuBar(barra);
+		opcionAbrir.addActionListener(this);
 		
 		
-		pack();
+		//PANEL
+		JPanel panelCentral = (JPanel) getContentPane();
+		Rectangle r=new Rectangle(900,600);
+		panelCentral.setBounds(r);
+		
+		//BOTON
+		JButton btnIntercalar=new JButton("Intercalar");
+		btnIntercalar.setMnemonic(KeyEvent.VK_I);
+		
+		
+		panel.add(barra,BorderLayout.NORTH);
+		//panel.add(panelCentral,BorderLayout.CENTER);
+		panel.add(btnIntercalar,BorderLayout.SOUTH);
+		//pack();
 		setLocationRelativeTo(null);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		elegir=new JFileChooser();
+		int seleccion=elegir.showSaveDialog(opcionAbrir);
+		if (seleccion == JFileChooser.APPROVE_OPTION)
+		{
+			File fichero= elegir.getSelectedFile();
+			try {
+				FileInputStream fc=new FileInputStream(fichero);
+				DataInputStream dt=new DataInputStream(fc);
+				while (true) {   
+	                int n = dt.readInt();  
+	                q.offer(n);
+	                
+				}
+			} catch (FileNotFoundException e1) {
+				
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		
 	}
 
 	}
